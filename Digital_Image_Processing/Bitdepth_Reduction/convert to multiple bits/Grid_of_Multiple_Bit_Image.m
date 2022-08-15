fbc4156e4af36b27a941f813f7d2d8b13cb1db7c
SUBRATA PANDEY <subra7a@gmail.com>
2022-08-15 22:27:03 +0530

    Update Grid_of_Multiple_Bit_Image.m

--- Digital_Image_Processing/Bitdepth_Reduction/convert to multiple bits/Grid_of_Multiple_Bit_Image.m
+++ Digital_Image_Processing/Bitdepth_Reduction/convert to multiple bits/Grid_of_Multiple_Bit_Image.m
@@ -15,3 +15,4 @@ for x=1:1:8 %Create a for loop from 1 to 8 with a gap of 1 e.g. 1,2,3...8
     subplot(2, 4, x) ; %Create axes in tiled positions for plotting all the eight images in a single graph
     imshow(fm_rounded,[]) %Convert the array 'fm_rounde' into an image with the range available in the array
 end %End of the for loop
+%End of the MATLAB code
